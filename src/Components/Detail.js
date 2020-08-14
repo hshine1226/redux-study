e6e58e01c58d3b75ffc647283598616f759ba5d0
hshine1226 <hshine1226@gmail.com>
2020-08-14 21:36:50 +0900

    Add Detail for practicing mapStateToProps

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,3 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default () => "Detail";
+function Detail(props) {
+  console.log(props);
+
+  return (
+    <>
+      <h1>{props.toDo?.id}</h1>
+      <h3>{props.toDo?.text}</h3>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  console.log(id);
+  return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo(props) {
+  console.log(props);
+  const { onBtnClick, text, id } = props;
+
   return (
-    <li>
-      {text}
-      <button onClick={onBtnClick}>DELETE</button>
-    </li>
+    <>
+      <li>
+        <Link to={`/${id}`}>{text}</Link>
+        <button onClick={onBtnClick}>DEL</button>
+      </li>
+    </>
   );
 }
 
